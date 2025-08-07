7b7db8ff915aace95c80fc4eb2fd295ab93aaf5d
Pratik Palwai <60940266+Pratik-Palwai@users.noreply.github.com>
2025-08-06 17:46:47 -0700

    Working polar slicer
    Polar slicer is working now, it is controlled by root/outboard chord, outboard offset, and wing half-span

--- slicer_4axis_polar.py
+++ slicer_4axis_polar.py
@@ -1,72 +1,75 @@
 import airfoil_library
 
-airfoil_file_path = airfoil_library.root_dir + "airfoils\\4412.afl"
-output_file_path = airfoil_library.root_dir + "gcodes\\wing_" + str(airfoil_library.time.time()) + "_polar.cnc"
+file_airfoil = airfoil_library.root_dir + "airfoils\\6415.afl"
 
 chord_root = 400 # mm
 chord_outboard = 300 # mm
-half_span = 914.4 # mm
-offset_outboard = 50 # positive values will put the outboard leading edge ahead of the root leading edge (mm)
-root_plane = 0 # 0 will put root chord on left tower (XY) and 1 will put it on right tower (AZ)
+outboard_offset = 25 # mm
+half_span = 609.6 # mm
 
 feedrate = 2000 # mm/min
-chord_height = 25.4 # height of the chord line above the bed
-tower_distance = 914.4 # distance between tower XY/AZ planes (mm)
+chord_height = 24.5 # mm
+tower_distance = 914.4 # mm
+root_plane = 0 # 0 places root on XY tower, 1 places root on AZ tower
 
-output_file_path = airfoil_library.root_dir + "gcodes\\wing_" + str(int(airfoil_library.time.time())) + "_polar.cnc"
+output_file_path = airfoil_library.root_dir + "gcodes\\wing_" + str(int(airfoil_library.time.time())) + "_polar_4axis.cnc"
 gcode_file = open(output_file_path, "w")
+file_extension = file_airfoil.split(".")[-1]
 
-points_root = airfoil_library.readFoil(afl_path=airfoil_file_path)[2]
-points_outboard = airfoil_library.readFoil(afl_path=airfoil_file_path)[2]
-
+points_root = airfoil_library.readFoil(afl_path=file_airfoil)[2]
 points_root = airfoil_library.setChord(airfoil_points=points_root, chord=chord_root)
-points_outboard = airfoil_library.setChord(airfoil_points=points_outboard, chord=chord_root)
-
 points_root = airfoil_library.applyOffset(airfoil_points=points_root, y_offset=chord_height)
-points_outboard = airfoil_library.applyOffset(airfoil_points=points_outboard, x_offset=offset_outboard, y_offset=chord_height)
 
-cartesian_points_root = []
-cartesian_points_outboard = []
+points_outboard = airfoil_library.readFoil(afl_path=file_airfoil)[2]
+points_outboard = airfoil_library.setChord(airfoil_points=points_outboard, chord=chord_outboard)
+points_outboard = airfoil_library.applyOffset(airfoil_points=points_outboard, x_offset=outboard_offset, y_offset=chord_height)
+
+points_opposite = []
 
 for i in range(len(points_root)):
-    cartesian_points_root.append([points_root[i][0], points_root[i][1], 0])
-    cartesian_points_outboard.append([points_outboard[i][0], points_outboard[i][1], half_span])
+    outboard_point = [points_outboard[i][0], points_outboard[i][1], half_span]
+    root_point = [points_root[i][0], points_root[i][1], 0]
+    
+    opposite_point = airfoil_library.oppositePoint(root_point, outboard_point, tower_distance)
+    points_opposite.append(opposite_point)
 
-gcode_file.write("; Airfoil: NACA " + airfoil_file_path[-8:-4] + "\n")
+gcode_file.write("; Airfoil: NACA " + file_airfoil[-8:-4] + "\n")
 gcode_file.write("; Root chord: " + str(chord_root) + " mm\n")
-gcode_file.write("; Outboard chord: " + str(chord_outboard) + " mm | Outboard offset: " + str(offset_outboard) + " mm\n")
-gcode_file.write("; Requested feedrate: " + str(feedrate) + "mm/s\n\n")
+gcode_file.write("; Outboard chord: " + str(chord_outboard) + " mm | Outboard offset: " + str(outboard_offset) + " mm\n")
+gcode_file.write("; Requested feedrate: " + str(feedrate) + "mm/s | Chord height: " + str(chord_height) + " mm\n\n")
 
 gcode_file.write("G21 ; Set units to millimeters\n")
-# gcode_file.write("G30 ; Home XYZA axes\n")
+# gcode_file.write("G30 ; Home XYAZ axes\n")
 gcode_file.write("G93 ; Activate inverse time motion mode\n")
 gcode_file.write("G90 ; Activate absolute coordinate system mode\n")
 # gcode_file.write("M3 S100; Set hot wire to 10% power\n")
-# gcode_file.write("G10 L20 P0 X0.000 Y0.000 A0.000 Z0.000 ; Set absolute coordinate system origin\n\n")
 
-[x_i, y_i] = [points_root[0][0], points_root[0][1]]
-[x_t, y_t] = [points_outboard[0][0], points_outboard[0][1]]
-[a_i, z_i] = airfoil_library.oppositePoint(point_root=cartesian_points_root[0], point_outboard=cartesian_points_outboard[0], plane_distance=tower_distance)
-delta = airfoil_library.inverseTime(delta_x=x_i, delta_y=y_i, delta_z=z_i, delta_a=a_i, middle_feedrate=2000)
+if root_plane == 0:
+    points_left = points_root
+    points_right = points_opposite
+elif root_plane == 1:
+    points_left = points_opposite
+    points_right = points_root
 
-gcode_file.write(airfoil_library.moveCommand(dx=x_i, dy=y_i, da=a_i, dz=z_i, f=delta))
+[x_i, y_i] = [points_left[0][0], points_left[0][1]]
+[a_i, z_i] = [points_right[0][0], points_right[0][1]]
+t_i = airfoil_library.inverseTime(delta_x=x_i, delta_y=y_i, delta_z=z_i, delta_a=a_i, middle_feedrate=2000)
+gcode_file.write(airfoil_library.moveCommand(x_i, y_i, a_i, z_i, t_i))
 
-for i in range(1, len(points_root)):
-    [x, y] = [points_root[i][0], points_root[i][1]]
-    [x_t, y_t] = [points_outboard[i][0], points_outboard[i][1]]
-    [a, z] = airfoil_library.oppositePoint(point_root=cartesian_points_root[i], point_outboard=cartesian_points_outboard[i], plane_distance=tower_distance)
+for i in range(1, len(points_left)):
+    [x, y] = [points_left[i][0], points_left[i][1]]
+    [a, z] = [points_right[i][0], points_right[i][1]]
 
-    [x_p, y_p] = [points_root[i - 1][0], points_root[i - 1][1]]
-    [x_p_t, y_p_t] = [points_outboard[i - 1][0], points_outboard[i - 1][1]]
-    [a_p, z_p] = airfoil_library.oppositePoint(point_root=cartesian_points_root[i - 1], point_outboard=cartesian_points_outboard[i - 1], plane_distance=tower_distance)
+    [x_p, y_p] = [points_left[i - 1][0], points_left[i - 1][1]]
+    [a_p, z_p] = [points_right[i - 1][0], points_right[i - 1][1]]
 
     dx = abs(x - x_p)
     dy = abs(y - y_p)
     da = abs(a - a_p)
     dz = abs(z - z_p)
-    dt = airfoil_library.inverseTime(dx, dy, da, dz, feedrate)
+    dt = airfoil_library.inverseTime(dx, dy, dz, da, feedrate)
 
-    gcode_file.write(airfoil_library.moveCommand(dx, dy, da, dz, dt))
+    gcode_file.write(airfoil_library.moveCommand(x, y, a, z, feedrate))
 
 gcode_file.write("\n")
 gcode_file.write("M5 ; Turn hotwire off\n")
