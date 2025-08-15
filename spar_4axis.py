a7c14217276c77efec544704f944f0551cfef481
Pratik Palwai <60940266+Pratik-Palwai@users.noreply.github.com>
2025-08-14 19:34:38 -0700

    Delete spar_4axis.py

--- spar_4axis.py
+++
@@ -1,8 +0,0 @@
-import airfoil_library
-
-spar_radius = 10 # mm
-spar_y = -25 # center of spar will be this far away from current position of wire
-
-output_file_path = airfoil_library.root_dir + "gcodes\\spar_" + str(int(airfoil_library.time.time())) + "_4axis.cnc"
-gcode_file = open(output_file_path, "w")
-
